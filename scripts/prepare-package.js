6ecae0a3cc231c59c5c0e2e44ae6613e7ef068b2
Erika McVey <erika.mcvey@blackbaud.com>
2019-10-08 10:45:36 -0400

    export styles, remove unnecessary exports and demo code

---
+++ scripts/prepare-package.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra');
+const sass = require('node-sass');
+const tildeImporter = require('node-sass-tilde-importer');
+const path = require('path');
+
+function copyScss() {
+  const result = sass.renderSync({
+    file: path.resolve(__dirname, '../src/app/public/styles/ag-grid-styles.scss'),
+    importer: tildeImporter
+  });
+
+  const target = path.resolve(__dirname, '../dist/css/sky-ag-grid.css');
+
+  fs.ensureFileSync(target);
+  fs.writeFileSync(target, result.css);
+}
+
+copyScss();
