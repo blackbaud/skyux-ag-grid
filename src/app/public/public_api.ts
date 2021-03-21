eaac4113b7a928bf6fec7c3d9811dbd86db2fa42
Thomas Ortiz <Thomasortiz95@gmail.com>
2021-03-21 03:28:31 -0400

    Export types

--- src/app/public/public_api.ts
+++ src/app/public/public_api.ts
@@ -1,11 +1,19 @@
 // Export any types that should be included in the root.
 export * from './modules/ag-grid/ag-grid.module';
 export * from './modules/ag-grid/ag-grid.service';
-export * from './modules/ag-grid/types/ag-grid-row-delete-cancel-args';
-export * from './modules/ag-grid/types/ag-grid-row-delete-confirm-args';
-export * from './modules/ag-grid/types/autocomplete-properties';
+
+// Core
 export * from './modules/ag-grid/types/cell-class';
 export * from './modules/ag-grid/types/cell-type';
-export * from './modules/ag-grid/types/datepicker-properties';
 export * from './modules/ag-grid/types/header-class';
 export * from './modules/ag-grid/types/sky-grid-options';
+
+// Editor Types
+export * from './modules/ag-grid/types/cell-editor-autocomplete-params';
+export * from './modules/ag-grid/types/autocomplete-properties';
+export * from './modules/ag-grid/types/datepicker-properties';
+
+// Renderer Types
+export * from './modules/ag-grid/types/ag-grid-row-delete-cancel-args';
+export * from './modules/ag-grid/types/ag-grid-row-delete-confirm-args';
+export * from './modules/ag-grid/types/cell-renderer-numeric-params';
