af92450cde3b7880480db33ca430d548f9f79d11
Thomas Ortiz <Thomasortiz95@gmail.com>
2021-03-21 03:45:24 -0400

    Export the numeric/autonumeric cells

--- src/app/public/public_api.ts
+++ src/app/public/public_api.ts
@@ -1,12 +1,17 @@
 // Export any types that should be included in the root.
+// Core
 export * from './modules/ag-grid/ag-grid.module';
 export * from './modules/ag-grid/ag-grid.service';
-
-// Core
 export * from './modules/ag-grid/types/cell-class';
 export * from './modules/ag-grid/types/cell-type';
 export * from './modules/ag-grid/types/header-class';
 export * from './modules/ag-grid/types/sky-grid-options';
+
+// Editors
+export * from './modules/ag-grid/cell-editors/cell-editor-autonumeric/cell-editor-autonumeric.component';
+
+// Renderers
+export * from './modules/ag-grid/cell-renderers/cell-renderer-numeric/cell-renderer-numeric.component';
 
 // Editor Types
 export * from './modules/ag-grid/types/cell-editor-autocomplete-params';
