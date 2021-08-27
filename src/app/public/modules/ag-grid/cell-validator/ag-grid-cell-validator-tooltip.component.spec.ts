90481958a85e47410146cb502dbba8e3a760ee99
John White <john.white@blackbaud.me>
2021-08-27 09:58:18 -0400

    Test refinements

--- src/app/public/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.spec.ts
+++ src/app/public/modules/ag-grid/cell-validator/ag-grid-cell-validator-tooltip.component.spec.ts
@@ -1,9 +1,10 @@
-import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { expect } from '@skyux-sdk/testing';
 import { SkyAgGridCellValidatorTooltipFixtureComponent } from '../fixtures/ag-grid-cell-validator-tooltip.component.fixture';
 import { SkyAgGridFixtureComponent } from '../fixtures/ag-grid.component.fixture';
 import { SkyAgGridFixtureModule } from '../fixtures/ag-grid.module.fixture';
 import { SkyCellClass } from '../types/cell-class';
+import { SkyAgGridCellValidatorTooltipComponent } from './ag-grid-cell-validator-tooltip.component';
 
 // Borrowed from skyux-lookup src/app/public/modules/lookup/lookup.component.spec.ts
 const isIE = window.navigator.userAgent.indexOf('rv:11.0') >= 0;
@@ -26,6 +27,40 @@ if (!isIE) {
       expect(fixture.componentInstance).toBeTruthy();
     });
 
+    it('should toggle popover', fakeAsync(() => {
+      const fixture = TestBed.createComponent(SkyAgGridCellValidatorTooltipComponent);
+      fixture.componentInstance.validatorMessage = 'Test message ABC';
+      fixture.componentInstance.parameters = {
+        // @ts-ignore
+        api: {
+          addEventListener(eventType: string, listener: Function) {
+          }
+        },
+        eGridCell: {
+          // @ts-ignore
+          addEventListener(eventType: string, listener: Function) {
+          }
+        },
+        // @ts-ignore
+        column: {
+          colId: () => -1
+        },
+        rowIndex: -1
+      };
+      fixture.detectChanges();
+      expect(fixture.componentInstance).toBeTruthy();
+      expect(fixture.componentInstance.indicatorShouldShow).toBeTrue();
+
+      fixture.componentInstance.showPopover();
+      tick();
+      expect(fixture.componentInstance.indicatorShouldShow).toBeFalse();
+
+      fixture.componentInstance.hidePopover();
+      fixture.componentInstance.showIndicator();
+      tick();
+      expect(fixture.componentInstance.indicatorShouldShow).toBeTrue();
+    }));
+
     it('should load a grid with validator columns', fakeAsync(() => {
       const gridFixture = TestBed.createComponent(SkyAgGridFixtureComponent);
       spyOn(gridFixture.componentInstance, 'gridReady');
@@ -35,86 +70,70 @@ if (!isIE) {
       expect(gridFixture.componentInstance.gridReady).toHaveBeenCalled();
     }));
 
-    it('should display an invalid number', fakeAsync(() => {
-      const gridFixture = TestBed.createComponent(SkyAgGridFixtureComponent);
-      gridFixture.detectChanges();
-      tick();
-      flush();
-
+    describe('events', () => {
       const cellSelector = `.${SkyCellClass.Number}.${SkyCellClass.Invalid}`;
+      let fixture: ComponentFixture<SkyAgGridFixtureComponent>;
+      let component: SkyAgGridFixtureComponent;
 
-      const cellElement = gridFixture.nativeElement.querySelector(cellSelector);
-      expect(cellElement).toBeTruthy();
-    }));
-
-    it('should refresh cells', fakeAsync(() => {
-      const gridFixture = TestBed.createComponent(SkyAgGridFixtureComponent);
-      gridFixture.detectChanges();
-      tick();
-      flush();
-
-      gridFixture.componentInstance.gridApi.refreshCells({
-        rowNodes: [
-          gridFixture.componentInstance.gridApi.getDisplayedRowAtIndex(3)
-        ]
+      beforeEach(() => {
+        fixture = TestBed.createComponent(SkyAgGridFixtureComponent);
+        component = fixture.componentInstance;
       });
 
-      gridFixture.detectChanges();
-      tick();
-      flush();
+      it('should trigger editing events', fakeAsync(() => {
+        fixture.detectChanges();
+        tick();
+        flush();
+        fixture.whenStable().then(() => {
+          const api = component.gridApi;
+          api.setFocusedCell(1, 'name');
+          tick();
+          flush();
+          api.startEditingCell({
+            colKey: 'nickname',
+            rowIndex: 1
+          });
+          tick();
+          flush();
+          api.stopEditing(true);
+          tick();
+          flush();
+          const cellElement = fixture.nativeElement.querySelector(cellSelector);
+          expect(cellElement).toBeTruthy();
+        });
+      }));
 
-      const cellSelector = `.${SkyCellClass.Number}.${SkyCellClass.Invalid}`;
+      it('should trigger key navigation events', fakeAsync(() => {
+        fixture.detectChanges();
+        tick();
+        flush();
+        fixture.whenStable().then(() => {
+          const api = component.gridApi;
+          const cellElement = fixture.nativeElement.querySelector(cellSelector);
+          expect(cellElement).toBeTruthy();
+          (cellElement as HTMLElement).dispatchEvent(new KeyboardEvent('keyup', {key: 'ArrowUp'}));
+          tick();
+          flush();
+          fixture.whenStable().then(() => {
+            let docElement: Element = cellElement.parentElement;
+            while (docElement.parentElement) {
+              docElement = docElement.parentElement;
+            }
+            const popoverElement = docElement.querySelector('.sky-popover-body');
+            expect(popoverElement).toBeTruthy();
 
-      const cellElement = gridFixture.nativeElement.querySelector(cellSelector);
-      expect(cellElement).toBeTruthy();
-    }));
-
-    it('should trigger events', fakeAsync(() => {
-      const gridFixture = TestBed.createComponent(SkyAgGridFixtureComponent);
-      gridFixture.detectChanges();
-      tick();
-      flush();
-
-      gridFixture.componentInstance.gridApi.setFocusedCell(1, 'name');
-
-      tick();
-      flush();
-
-      gridFixture.componentInstance.gridApi.startEditingCell({
-        colKey: 'nickname',
-        rowIndex: 1
-      });
-
-      tick();
-      flush();
-
-      const cellSelector = `.${SkyCellClass.Number}.${SkyCellClass.Invalid}`;
-
-      const cellElement = gridFixture.nativeElement.querySelector(cellSelector);
-      expect(cellElement).toBeTruthy();
-      (cellElement as HTMLElement).dispatchEvent(new KeyboardEvent('keyup', {key: 'ArrowUp'}));
-
-      tick();
-      flush();
-
-      let docElement: Element = cellElement.parentElement;
-      while (docElement.parentElement) {
-        docElement = docElement.parentElement;
-      }
-      const popoverElement = docElement.querySelector('.sky-popover-body');
-      expect(popoverElement).toBeTruthy();
-
-      gridFixture.componentInstance.gridApi.startEditingCell({
-        colKey: 'validNumber',
-        rowIndex: 3
-      });
-
-      tick();
-      flush();
-
-      const editorElement = gridFixture.nativeElement.querySelector(`${cellSelector}.ag-cell-inline-editing`);
-      expect(editorElement).toBeTruthy();
-    }));
+            api.startEditingCell({
+              colKey: 'validNumber',
+              rowIndex: 3
+            });
+            tick();
+            flush();
+            const editorElement = fixture.nativeElement.querySelector(`${cellSelector}.ag-cell-inline-editing`);
+            expect(editorElement).toBeTruthy();
+          });
+        });
+      }));
+    });
   });
 
 }
